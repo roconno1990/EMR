36ff462be539a2420f0ad63cdf5d92d59c2f75b9
Ryan O'Connor <roconno1990@gmail.com>
2018-01-26 09:02:33 -0500

    Addition of new table
    Addition of new patient table.

--- baseline/database.sql
+++ baseline/database.sql
@@ -9,3 +9,19 @@ CREATE TABLE USER (
 	username      VARCHAR(30) not null,
 	password      VARCHAR(30) not null,
 	role          VARCHAR(30) not null );
+
+drop table if exists PATIENT;
+CREATE TABLE PATIENT (
+    patient_id          VARCHAR(10) PRIMARY KEY,
+    update_time         TIMESTAMP,
+    first_name          VARCHAR(30) not null,
+    middle_name         VARCHAR(30),
+    last_name           VARCHAR(30) not null,
+    med_rec_ref         VARCHAR(10) not null,
+    finance_ref         VARCHAR(10) not null,
+    social_Security     VARCHAR(10) not null,
+    address             VARCHAR(100),
+    attending_physician VARCHAR(30),
+    caring_nurse        VARCHAR(30),
+    room_num            VARCHAR(10),
+    location            VARCHAR(50) );
